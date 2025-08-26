917d092312b49e05e510b1182b8837f92268be87
TannerG7 <girard36262@students.apsb.org>
2025-08-26 18:48:36 +0000

    added more things to platformer

--- fsd-projects/platformer/platformer.js
+++ fsd-projects/platformer/platformer.js
@@ -27,31 +27,33 @@ $(function () {
     //////////////////////////////////
 
     // TODO 1 - Enable the Grid
-    // toggleGrid();
+     toggleGrid();
     
 
     // TODO 2 - Create Platforms
-createPlatform(150 , 650 , 200, 20,"blue");
-createPlatform(400 , 550 , 170, 20,"blue" );
-createPlatform(650 , 450 , 150, 20, "green");
-createPlatform(850 , 350 , 125, 20, "green");
-createPlatform(1200 , 255, 115, 20, "cyan");
-createPlatform( 500, 300, 115, 20, "red");
+createPlatform(150 , 650 , 200, 10,"blue");
+createPlatform(400 , 550 , 170, 15,"blue" );
+createPlatform(650 , 450 , 150, 15, "green");
+createPlatform(850 , 350 , 125, 15, "green");
+createPlatform(1200 , 255, 125, 15, "cyan");
+createPlatform( 500, 300, 115, 15, "red");
 createPlatform(200 , 255, 200, 20, "cyan");
-createPlatform(1075 , 500, 115, 20, "cyan");
+createPlatform(1075 , 500, 115, 15, "cyan");
 // TODO 3 - Create Collectables
   createCollectable("steve", 1100, 450);
 createCollectable("diamond", 200, 170, 0, 0.7);
 createCollectable("database", 1250, 200, 0, 0.7);
-
+createCollectable("max", 1200, 150, 0, 1, 1000, 1200, 2);
 
     
-    // TODO 4 - Create Cannons
+  // TODO 4 - Create Cannons
 
 createCannon("top", 300, 1700);
-createCannon("right", 200, 2000);
+createCannon("right", 495, 2000);
 createCannon("bottom", 1050, 3000);
-    createCannon("left", 150, 3000);
+  createCannon("left", 145, 3000);
+
+
     //////////////////////////////////
     // ONLY CHANGE ABOVE THIS POINT //
     //////////////////////////////////
